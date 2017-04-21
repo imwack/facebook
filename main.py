4ba3b3d96a5fba7ad10af887440ec9007c0a9868
wack <kai.wang@citrix.com>
2017-04-21 11:36:28 +0800

    init

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #coding=utf-8
 import snap
 
+def GetDegree(UGraph):
+    """
+    获取图 度-节点数目
+    :param UGraph: 
+    :return: 
+    """
+    DegToCntV = snap.TIntPrV()
+    snap.GetDegCnt(UGraph, DegToCntV)
+    for item in DegToCntV:
+        print "%d nodes with degree %d" % (item.GetVal2(), item.GetVal1())
+
 if __name__ == '__main__':
     FIn = snap.TFIn("./facebook_combined/facebook.graph")
     G = snap.TUNGraph.Load(FIn)
-    print G.GetEdges()  #Total Edges
+    #print G.GetEdges()  #Total Edges
+    GetDegree(G)
